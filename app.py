653b31a5df8e516e482ac4849c464a50e8303e9d
12061988 <m.maguettefall@gmail.com>
2023-02-16 17:20:44 +0000

    add new code to data management.py

--- app.py
+++ app.py
@@ -1,17 +1,23 @@
 import streamlit as st
-from multi_page_app.multi_page import MultiPage
-from multi_page_app.page_summary import page_summary_body
-from multi_page_app.Mildew_and_Healthy_Visualizer import page_cells_visualizer_body
-from multi_page_app.Mildew_detector import page_mildew_detector_body
-from multi_page_app.project_hypothesis import page_project_hypothesis_body
-from multi_page_app.ml_performance import page_ml_performance_metrics
+from app_pages.multi_page import MultiPage
 
-app = MultiPage(app_name= "Detection of deseased leaves")
+# load pages scripts
 
-app.add_page("Quick Project Summary", page_summary_body)
-app.add_page("Cherry leave Visualizer", page_cells_visualizer_body)
-app.add_page("Mildew Detection", page_mildew_detector_body)
-app.add_page("Project Hypothesis", page_project_hypothesis_body)
-app.add_page("ML Performance Metrics", page_ml_performance_metrics)
+from app_pages.page_project_summary import page_project_summary_body
+from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_leaves_visualizer import page_leaves_visualizer_body
+from app_pages.page_ml_performance import page_ml_performance_body
+from app_pages.page_mildew_detector import page_mildew_detector_body
 
-app.run() # Run the  app
+
+app = MultiPage(app_name="Detection of deseased leaves")
+
+
+app.add_page("Project Summary", page_project_summary_body)
+app.add_page("Project Hypothesis and Validation", page_project_hypothesis_body)
+app.add_page("Cherry Leaf Visualizer", page_leaves_visualizer_body)
+app.add_page('Mildew Detector', page_mildew_detector_body)
+app.add_page('ML Performance Metric', page_ml_performance_body)
+
+app.run()
+
